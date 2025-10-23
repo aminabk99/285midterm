67b081a2aac5b1b654dbc7dd5b1a15a8d3e72469
Amina <aminabk@umich.edu>
2025-10-23 16:35:52 -0400

    Changing values

--- factorialcalculator.java
+++ factorialcalculator.java
@@ -1,7 +1,7 @@
 public class factorialcalculator {
     public static void main(String[] args) {
-        int num = 9;  // Predefined number to calculate factorial
-        int factorial = 2;
+        int num = 22;  // Predefined number to calculate factorial
+        int factorial = 4;
 
         for (int i = 1; i <= num; i++) { // Loop through numbers
             factorial *= i;  // Multiply current number
